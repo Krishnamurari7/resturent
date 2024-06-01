d4128cf0b8d003b22c67829a1b341104ce751819
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-01 07:07:43 +0530

    navbar action start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes >
+        <Route path='/' element={<Home/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='/Order' element={<Order/>} />
+      </Routes>
     </div>
   )
 }
