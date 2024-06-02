6dadae68b9f0dd1176e5da8c45dc9a593c4647b4
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-02 07:38:36 +0530

    footer setup

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id="footer">
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dicta earum, veritatis quam repellendus sit pariatur nam beatae reiciendis repellat odio porro ab dolorem ad debitis. Quia veniam beatae quasi?</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+91 7488684917</li>
+                    <li>Contact@km.com</li>
+                </ul>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
