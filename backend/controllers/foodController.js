c8c1985856fab8e602e858a0c8fb61bbf4cb2d6e
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-05 06:29:34 +0530

    backend setup almost done

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -26,4 +26,31 @@ const addFood = async (req, res) => {
 
 }
 
-export {addFood}
+//all food list
+
+const listFood = async (req, res) =>{
+    try{
+        const foods = await foodModel.find({});
+        res.json({sucess:true,data:foods})
+    }catch(error){
+        console.log(error);
+        res.json({sucess:false,message:"Error"})
+    }
+}
+
+//remove food item
+
+const removeFood = async(req,res)=>{
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({sucess:true,message:"Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({sucess:false,message:"error"})
+    }
+}
+
+export {addFood,listFood,removeFood}
