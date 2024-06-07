dc271ba94ee3d9fb1a0c88586ef21663e0b249fe
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-07 07:08:29 +0530

    register method is completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,17 +6,69 @@ import validator from "validator";
 
 
 
+
 //login user
 const  loginUser = async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false,message:"User Doesn't exist"})
+        }
 
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if (!isMatch) {
+            return res.json({success:false,message:"Invalid credentials"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+const createToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET)
 }
 
 //register user
 
 const registerUser = async (req,res) => {
+    const {name,email,password} = req.body;
+    try {
+        //checking all user exists
+        const exists = await userModel.findOne({email});
+        // console.log(userModel.findOne({email}));
+         if (exists) {
+        return res.json({success:false,message:"User already exits"})
+         }
+         //vallidating email formt & strong Password
+         if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Please valid email"})
+         }
+         if(password.length<8){
+            return res.json({sucess:false,message:"Password must be at least 8 characters"})
+         }
 
+         //hashing password
+         const salt = await bcrypt.genSalt(10);
+         const hashedPassword = await bcrypt.hash(password,salt);
+         const newUser = new userModel({name:name,email:email,password:hashedPassword});
+         const user = await newUser.save();
+         const token = createToken(user._id)
+         res.json({sucess:true,token})
+
+
+    } catch(error){
+        console.log(error);
+        res.json({success:false,message:"strong password"})
+
+    }
 }
 
 
-
-export default {loginUser,registerUser};
+export {loginUser,registerUser};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
-import express from 'express'
+import express from 'express';
 
 import {loginUser, registerUser } from "../controllers/userController.js"
 
 
+const userRouter = express.Router()
+
 userRouter.post('/login', loginUser)
-userRouter.post('/register', registerUser)
+userRouter.post('/registr', registerUser)
 
 export default userRouter;
