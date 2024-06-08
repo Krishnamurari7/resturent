76c643d5e1c3545322292a9bb4352837c95957e4
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-06-08 05:44:47 +0530

    small update

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import './Navbar.css'
 import { assets } from '../../assets/assets';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 // import LoginPopup from '../LoginPopup/LoginPopup';
 
@@ -10,7 +10,17 @@ const Navbar = ({setShowLogin}) => {
 
     const[menu,setMenu] = useState("Home")
 
-    const {getTotalCartAmount,token,setToken} = useContext(StoreContext)
+    const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
+
+
+    const navigate = useNavigate();
+
+    const logout = () =>{
+        localStorage.removeItem("token");
+        setToken("");
+        navigate("/")
+
+    }
 
 
   return (
@@ -34,7 +44,7 @@ const Navbar = ({setShowLogin}) => {
                 <ul className="nav-profile-dropdown">
                     <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
                     <hr/>
-                    <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+                    <li onClick={logout()}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
                 </ul>
                 </div>}
             
